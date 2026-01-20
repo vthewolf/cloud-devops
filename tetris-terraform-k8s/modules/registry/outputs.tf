efffa53e9f613858a56fa0cd665ab2d6d93b34e7
Víctor SF. <79378136+vthewolf@users.noreply.github.com>
2026-01-20 08:08:02 +0100

    Delete tetris-terraform-k8s directory

--- tetris-terraform-k8s/modules/registry/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "repository_url" {
-description = "La URL donde debemos subir nuestra imagen Docker"
-value       = aws_ecr_repository.this.repository_url
-}
-
-output "repository_arn" {
-value = aws_ecr_repository.this.arn
-}
